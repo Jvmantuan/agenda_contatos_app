2ed899594df1493bc796fbb95dbc00b5e6c5e36a
Joao Victor Oliveira <jvoliveira3011@gmail.com>
2020-06-30 19:49:16 -0300

    App Finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:agendacontatosapp/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:agendacontatosapp/ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
